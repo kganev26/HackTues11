27c4cda3e80dcbbade9a8dbc024959a2059ee642
Petar Kavrakov <petikavrakov@gmail.com>
2025-03-21 19:01:37 +0200

    HTMLIBOT

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './Patenca.png';
 import './App.css';
+import Chat from './Chat';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';  
 
@@ -9,46 +10,47 @@ const App = () => {
 
   return (
     <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
+      
+      <li>
+            <Link to="/">Contact</Link>
           </li>
-          <li>
-            <Link to="/login">login</Link>
-          </li>
-          <li>
-            <Link to="/register">register</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <div class="nav_background">
+    <div class="nav_background">
     <nav>
       <div class="logo">
         <a href="#"><span>My</span>cons</a>
       </div>
-      <div class="buttons">
+      <div class="buttons"> 
         <a href="#" class="login">Log in</a>
         <a href="#" class="btn">Register</a>
       </div>
     </nav>
   </div>
-  <div class="container">
+  <div class="container"> 
     
     <div class="content">
       <h2>Bring AI to your<br></br>daily financial decisions</h2> 
-      <p>With your ai consultant having access to your monthly incomes and expenses, he can help you decide wether a certian purchase or investment is wirth it at the moment.</p>
+      <p>With your ai consultant having access to your monthly incomes and expenses, he can help you decide wether a certian purchase or investment is really worth it.</p>
     </div>
     <div class="link">
       <a href="#" class="hire">Try It</a>
     </div>
   </div>
+  <div class="contact">
+    <div class="content">
+    <h1>Contact Us</h1>
+    <div class="contactlinks">
+      <a href="https://mail.google.com/mail/?view=cm&to=rozovitepatenca@gmail.com&subject=Your%20Subject&body=Your%20Message" target="_blank">Email Us</a><br></br>
+      <a href="https://x.com/Rozovitepatenca" target="_blank">Follow us on X</a><br></br>
+      <a href="https://www.instagram.com/rozovitepatenca/" target="_blank">Follow us on Instagram</a>
+    </div>
+    </div>
+  </div>
+  <div>
+    <Chat />
+  </div>
+  </div>
+  
 
-      </div>
-      
-
-    
   );
 }
 
